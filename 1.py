21d32941882db0bca4f8360fa16d7ed04227ad36
What1sLov3RK <72340160+What1sLov3RK@users.noreply.github.com>
2021-09-30 11:33:31 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -2,21 +2,21 @@
 # Also, provide setter and getter for the length and width attributes. The setter should verify that length and width are each floating-point numbers larger than 0.0 and less than 20.0.
 class Rectangle(object):
     def __init__(self,length=1,width=1):
-        self.length=length
-        self.width=width
         if length>0 and length< 20:
             pass
         else :
-            self.length=1
+            length=1
         if width > 0 and width < 20:
             pass
         else:
-            self.width = 1
+            width = 1
+        self.length=length
+        self.width=width
     def perimeter(self):
         return (self.length+self.width)*2
     def area(self):
         return self.length*self.width
 
-rect = Rectangle(6,3)
+rect = Rectangle(22,3)
 print(rect.perimeter())
 print(rect.area())
